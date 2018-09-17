3f4dbfc4d9f063e5c7fec033989a7dc572055208
Ricardo Bugan <dones.bugan@hotmail.com>
2018-09-17 11:58:21 -0300

    Video 1.3

--- Assets/Scripts/UI/BotaoAnalogico.cs
+++ Assets/Scripts/UI/BotaoAnalogico.cs
@@ -1,12 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class BotaoAnalogico : MonoBehaviour, IDragHandler
 {
-    public void OnDrag(PointerEventData eventData)
+    [SerializeField]
+    private RectTransform imagemFundo;
+
+    public void OnDrag(PointerEventData dadosDoMouse)
     {
-        Debug.Log("Arrastando");
+        var posicaoMouse = CalcularPosicaoMouse(dadosDoMouse);
+        Debug.Log(posicaoMouse);
+    }
+
+    private Vector2 CalcularPosicaoMouse(PointerEventData dadosDoMouse)
+    {
+        Vector2 posicao;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            imagemFundo,
+            dadosDoMouse.position,
+            dadosDoMouse.enterEventCamera,
+            out posicao
+            );
+
+        return posicao;
     }
 }
