090b16383e7f7253b20cb92bf96927eeb30c6cfe
Ricardo Bugan <dones.bugan@hotmail.com>
2018-09-18 15:20:51 -0300

    Video 1.4

--- Assets/Scripts/UI/BotaoAnalogico.cs
+++ Assets/Scripts/UI/BotaoAnalogico.cs
@@ -8,11 +8,20 @@ public class BotaoAnalogico : MonoBehaviour, IDragHandler
 {
     [SerializeField]
     private RectTransform imagemFundo;
+    [SerializeField]
+    private RectTransform imagemBolinha;
+   
 
     public void OnDrag(PointerEventData dadosDoMouse)
     {
         var posicaoMouse = CalcularPosicaoMouse(dadosDoMouse);
+        this.PosicionarJoystick(posicaoMouse);
         Debug.Log(posicaoMouse);
+    }
+
+    private void PosicionarJoystick(Vector2 posicaoMouse)
+    {
+        this.imagemBolinha.localPosition = posicaoMouse;
     }
 
     private Vector2 CalcularPosicaoMouse(PointerEventData dadosDoMouse)
